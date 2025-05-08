4957b92a4ce92e182bdcfd582363b500bfdd23e8
João Emanuel <108814202+JOAO2666@users.noreply.github.com>
2025-05-08 15:16:12 -0300

    Add files via upload

---
+++ Aulas/08-05-2025/-ponteiros com %p.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char *argv[]) {
+    int x;
+    int *p;
+    x = 10;
+ 	p = &x;
+    *p = *p + 7;
+    printf("x = %d\n", x);
+    printf("p = %p\n", p);
+    printf("*p = %d\n", *p);
+    printf("&x = %p\n", &x);
+    printf("&x = %p\n", &p);
+    return 0;
+}
